664e8ba8def0ec882ef9e187189fa91390ba71a2
mrvjns <joensmarvin@icloud.com>
2025-01-11 16:31:57 +0100

    push changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import { LpNavbar1 } from "@/components/lp-navbar-1";
-import { HeroSection4 } from "@/components/hero-section-4";
-import { FeatureSection9 } from "@/components/feature-section-9";
-import { Footer3 } from "@/components/footer-3";
-import { FaqSection1 } from "@/components/faq-section-1";
-import { Banner1 } from "@/components/banner-1";
-
 export default function Home() {
   return (
     
